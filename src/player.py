8ca1de494be77d1be5d0125f82e52193aaa54d87
Vyraal1 <kevin.afable@gmail.com>
2020-02-13 22:15:00 -0500

    feat: player methods and attrs

--- src/player.py
+++ src/player.py
@@ -1,2 +1,33 @@
+import functools
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, initial_room):
+        self.name = name
+        self.current_room = initial_room
+        self.inventory = []
+
+    def move(self, direction):
+        try:
+            self.room = getattr(self.room, direction)
+        except AttributeError:
+            print("The room doesn't exist or movement isn't allowed.")
+
+    def print_items(self):
+        if len(self.inventory) > 0:
+            print("You see the following items in front of you:")
+            for item in self.inventory:
+                print(item)
+        else:
+            print("You have nothing in your inventory.")
+
+    def drop(self, ind):
+        dropped = self.inventory.pop(ind)
+        self.current_room.append(dropped)
+        self.print_items()
+
+    def grab(self, ind):
+        grabbed = self.current_room.pop(ind)
+        self.inventory.append(grabbed)
