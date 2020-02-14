58c13e2a4a2bd90750a88873942ed90d26dcb1bc
Vyraal1 <kevin.afable@gmail.com>
2020-02-13 22:14:48 -0500

    feat: expanded room

--- src/room.py
+++ src/room.py
@@ -1,2 +1,26 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    """ """
+
+    def __init__(self, name, desc, items=[]):
+        self.name = name
+        self.desc = desc
+        self.items = items
+        self.n = None
+        self.s = None
+        self.e = None
+        self.w = None
+
+    def __str__(self):
+        return f"Name: {self.name}, {self.desc}, items: {self.items}"
+
+    def print_items(self):
+        if len(self.items) > 0:
+            print("You see the following items in front of you:")
+            for index, item in enumerate(self.items):
+                print(f"Slot {index}: {item}")
+        else:
+            print("You don't notice anything of interest.")
